73b2743108446403974a4fa389544caf217c3f5b
Travis Kosarek <travis.kosarek@gmail.com>
2018-01-27 10:07:32 -0600

    initial commit of example game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector3 offset;
+    public GameObject Player;
+
+    // Use this for initialization
+    void Start () {
+        this.offset = this.transform.position - Player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        this.transform.position = Player.transform.position + this.offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rigidBody;
+    private int count;
+
+    public float Speed;
+    public Text CountText;
+    public Text WinText;
+
+    private void Start()
+    {
+        this.rigidBody = GetComponent<Rigidbody>();
+        this.count = 0;
+        this.updateCountText();
+        this.WinText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        this.rigidBody.AddForce(movement * this.Speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            this.count++;
+            this.updateCountText();
+        }
+    }
+
+    private void updateCountText()
+    {
+        this.CountText.text = "Count: " + count.ToString();
+		if(count >= 10){
+            this.WinText.text = "You Win!";
+        }
+	}
+
+}
